7c81598a75d4846beca334c3922628577f39f963
Evgheni Nujnenco <nev.front.dev@gmail.com>
2026-01-28 10:15:44 +0200

    added home page ro/ru versions

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/useRudderAnalytics.ts -> @rudderstack/analytics-js\":{\"id\":\"app/useRudderAnalytics.ts -> @rudderstack/analytics-js\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_rudderstack_analytics-js_dist_npm_modern_esm_index_mjs.js\"]}}"
