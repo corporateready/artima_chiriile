082a803e3ac8236b03739f36df19ac2b4d74f9f2
Evgheni Nujnenco <nev.front.dev@gmail.com>
2026-01-28 10:53:47 +0200

    added home page ro/ru versions

--- app/api/googlePlaces/route.js
+++ app/api/googlePlaces/route.js
@@ -1,6 +1,7 @@
+
 export async function GET(request) {
 
-  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${process.env.NEXT_PUBLIC_GOOGLE_MAP}`;
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJS2gzy9p9yUARa-ZmvFU-kd8&key=AIzaSyDBwXJ1SBcSfzRlZOr0NTi5xSHhCobzHs4`;
 
   try {
     const response = await fetch(url);
